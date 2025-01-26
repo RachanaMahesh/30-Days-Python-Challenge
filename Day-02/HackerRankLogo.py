87d587061a0fa2452d6455480b9c89fc9f8af2c7
Rachana Mahesh <rachanamahesh9@gmail.com>
2025-01-27 00:53:15 +0530

    Added solutions for Day-02

--- Day-02/HackerRankLogo.py
+++ Day-02/HackerRankLogo.py
@@ -1,4 +1,37 @@
-n = int(input("Enter the thickness"))
+# n = int(input("Enter the thickness"))
+# c = 'H'
+# for i in range(n):
+#     print((c*i).rjust(n-1)+c+(c*i).ljust(n-1))
+# for i in range(n+1):
+#     print((c*n).center((n*2)-1)+(c*0).center((n*2)-1)+(c*n).center((n*2)-1))
+# for i in range((n+1)//2):
+#     print((c*(n*5)).center((n*6)-1))
+# for i in range(n+1):
+#     print((c*n).center((n*2)-1)+(c*0).center((n*2)-1)+(c*n).center((n*2)-1))
+# for i in range(n):
+#     print(((c*(n-i-1)).rjust(n-1)+c+(c*(n-i-1)).ljust(n-1)).rjust((n*6)-3))
+
+#Replace all ______ with rjust, ljust or center. 
+
+thickness = int(input()) #This must be an odd number
 c = 'H'
-for i in range(n):
-    print((c*i).rjust(n-1)+c+(c*i).ljust(n-1))
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
