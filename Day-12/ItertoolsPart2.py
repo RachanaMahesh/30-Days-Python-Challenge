f9453bc5f538f97921bd824338d8de4abcec1b85
Rachana Mahesh <rachanamahesh9@gmail.com>
2025-03-14 00:14:19 +0530

    Itertools part2

--- Day-12/ItertoolsPart2.py
+++ Day-12/ItertoolsPart2.py
@@ -1,4 +1,5 @@
 import itertools
+import operator
 # 10. chain() : allows us to chain together the iterables so that we will go through all the items in the 1st iterable & then followed by 2nd iterable and so on
 print("----------------------  10. chain()    ------------------------")
 letters = ['a','b','c','d']
@@ -30,13 +31,13 @@ with open('test.log','r') as f: # files are actually iterators, when you u call 
     for line in header:
         print(line,end="") # as it as \n after each line if we add just print then we add 2 lines
 
-print("---------------------------  13. compress()  ------------------------------------------------")
-#13. compress(): 
+print("---------------------------  12. compress()  ------------------------------------------------")
+#13. compress(): func used is datascience problems where u can pass iterables & some selector & it will retuen a new iterable that only containes items in our iterable that has a corresponding True value.
 setters =[True,True,False,True]
 result = itertools.compress(letters,setters)
 print(list(result))
 
-#filter():
+#filter(): it ia aimilR TO COMPRESS but the difference is that filter() uses a function to determine whether something is true or false but in compress those values were passed in as iterable
 print("--------------------------- filter()  example------------------------------------------------")
 def lt2(num):
     if num < 2:
@@ -48,36 +49,43 @@ for item in result1:
     print(item, end =" ")
 print()
 
-print("---------------------------  14. filterfalse()  ------------------------------------------------")
-# filterfalse():
+print("---------------------------  13. filterfalse()  ------------------------------------------------")
+# filterfalse(): it is similar to filter() func except instead it gives you the values that retun false instead of true 
 result2 = itertools.filterfalse(lt2,num)
 for item in result2:
     print(item, end =" ")
 print()
 
-print("---------------------------  15. dropwhile()  ------------------------------------------------")
-# dropwhile():
+print("---------------------------  14. dropwhile()  ------------------------------------------------")
+# dropwhile(): drops the first few ones that met the criteria 
 result2 = itertools.dropwhile(lt2,num)
 for item in result2:
     print(item, end =" ")
 print()
 
-print("---------------------------  16. takewhile()  ------------------------------------------------")
-# takewhile():
+print("---------------------------  15. takewhile()  ------------------------------------------------")
+# takewhile(): instead will grab all the values that retuned true  & as soon as it hits a value that doesn't return true then it will just stop at that point
 result3 = itertools.takewhile(lt2,num)
 for item in result3:
     print(item, end =" ")
 print()
 
-print("---------------------------  17. accumulate()  ------------------------------------------------")
-#accumulate(): 
+print("---------------------------  16. accumulate()  ------------------------------------------------")
+#accumulate(): takes an iterable & returns accumulated sum of each item & it uses addition by default 
 result4 = itertools.accumulate(num)
 for item in result4:
     print(item, end =" ")
 print()
+# we can use other operatior as well i.e., multiply
+n =[1,3,5,6,7]
+multiples_result = itertools.accumulate(n, operator.mul)
+for item in multiples_result:
+    print(item, end =" ")
+print()
 
-print("---------------------------  18. groupby()  ------------------------------------------------")
-# groupby(): 
+print("---------------------------  17. groupby()  ------------------------------------------------")
+# groupby(): go through an iterable & group value based on a certain key & then it will return a stream of tuples 1. key 2. an iterator that were grouped by that key
+
 def get_state(person):
     return person['state']
 
@@ -135,10 +143,12 @@ for key,group in person_group:
     #     print("Person:",person)
     print('Count:',len(list(group)))
     print()
+# we can use these to fetch how many student in a class got A, B & C...
+#Group by () expects the initial iterable to be sortd so that it can group properly which is different from SL-group by 
 
-print("---------------------------  19. tee()  ------------------------------------------------")
-#tee(): 
-# people.sort(key=get_state)
+print("---------------------------  18. tee()  ------------------------------------------------")
+#tee(): when we want to replicate an iterator 
+people.sort(key=get_state)
 
 # Step 2: Convert groupby iterator to a list of tuples (state, list of people)
 person_group_list = [(state, list(group)) for state, group in itertools.groupby(people, get_state)]
@@ -153,5 +163,17 @@ for state, group in copy1:
     for person in group:
         print("  ", person)
 
+# if we want more copie then we could just pass in an arguement to this tee func & return however many that we'd like to have
+# #NOTE: we shld no longer use the original iterator after u copy it.i.e., we shldn't use person_group_list but can use copy1 & copy2
 
-
+x=[1,2,3,4]
+x1,x2,x3 = itertools.tee(x,3)  
+for i in x1:
+    print(i,end=  " ")
+print()
+for i in x2:
+    print(i,end=  " ")
+print()
+for i in x3:
+    print(i,end=  " ")
+print()
