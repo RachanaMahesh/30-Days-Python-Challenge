5145569c155d04ce673805cb8ce832f76bb1e942
RachanaMahesh <155420126+RachanaMahesh@users.noreply.github.com>
2025-04-12 22:38:37 +0530

    Delete 30-Days-Python-Challenge directory

--- 30-Days-Python-Challenge/Day-01/RemoveLettersFromaString.py
+++
@@ -1,5 +0,0 @@
-s = "Hello World"
-# s = s.replace('l','')
-# To remove First oc
-i = s.find('o')
-print(s[:i]+s[i+1:])
--- 30-Days-Python-Challenge/Day-01/ReverseWordInString.py
+++
@@ -1,3 +0,0 @@
-s = input("Enter a String:")
-rev = (" ".join(s.split()[::-1]))
-print(rev)
